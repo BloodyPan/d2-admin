a14449aa7fd501a07b8beeb671e8967aaee96349
Lu <vongolaluan@hotmail.com>
2018-08-26 00:17:32 +0800

    bug fixed

--- src/pages/user/new-user/index.vue
+++ src/pages/user/new-user/index.vue
@@ -58,7 +58,7 @@ export default {
     }
   },
   methods: {
-    timeFormat: time => util.formatTimestamp(time, 'yyyy-MM-dd hh:mm:ss'),
+    timeFormat: time => util.spot.formatTimestamp(time, 'yyyy-MM-dd hh:mm:ss'),
     tableRowClassName ({row, rowIndex}) {
       if (rowIndex % 2 === 1) {
         return 'success-row'
